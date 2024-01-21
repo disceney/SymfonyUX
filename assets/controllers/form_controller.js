a353ad667403b213c56222adac5d06f89c512410
Adrien B. <adrienbisiaux7@gmail.com>
2024-01-21 15:25:52 +0100

    It's work #1

--- assets/controllers/form_controller.js
+++ assets/controllers/form_controller.js
@@ -1,20 +1,42 @@
 import { Controller } from "@hotwired/stimulus";
+import { getComponent } from "@symfony/ux-live-component";
 import Choices from "choices.js";
+import flatpickr from "flatpickr";
 import $ from "jquery";
 
 export default class extends Controller {
-	connect () {
-		this.initSelectInput();
+	async initialize () {
+		this.component = await getComponent(this.element);
+		
+		this.component.on("render:finished", (component) => {
+			this.initPluginInput();
+		});
 	}
 	
-	initSelectInput () {
+	connect () {
+		this.initPluginInput();
+	}
+	
+	initPluginInput () {
 		let selectInput = $("form select");
 		
 		if (selectInput.length > 0) {
 			selectInput.each(function () {
-				new Choices(this, {
-					allowHTML: true
-				});
+				if ($(this).hasClass("choices__input") === false) {
+					new Choices(this, {
+						allowHTML: true
+					});
+				}
+			});
+		}
+		
+		let dateInput = $("form input[type='date']");
+		
+		if (dateInput.length > 0) {
+			dateInput.each(function () {
+				if ($(this).hasClass("flatpickr-input") === false) {
+					flatpickr(this);
+				}
 			});
 		}
 	}
--- src/Form/ExampleType.php
+++ src/Form/ExampleType.php
@@ -5,11 +5,13 @@ namespace App\Form;
 use App\Traits\DataTrait;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Choice;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
 
 class ExampleType extends AbstractType
 {
@@ -18,6 +20,7 @@ class ExampleType extends AbstractType
 		$builder->add("name", TextType::class, [
 			"label" => "Name",
 			"constraints" => [
+				new NotBlank(),
 				new Length([
 					"min" => 6,
 					"max" => 124,
@@ -25,15 +28,47 @@ class ExampleType extends AbstractType
 			]
 		]);
 
-
-		$builder->add("choices", ChoiceType::class, [
-			"label" => "Choices",
+		$builder->add("choices_1", ChoiceType::class, [
+			"label" => "Choices 1",
 			"choices" => DataTrait::getData(),
 			"constraints" => [
 				new NotBlank(),
 				new Choice([
 					"choices" => DataTrait::getData(),
 				])
+			],
+			"attr" => [
+				"data-live-id" => "form-choices-1"
+			],
+			"empty_data" => "Element 0"
+		]);
+
+		$builder->add("choices_2", ChoiceType::class, [
+			"label" => "Choices 2",
+			"choices" => DataTrait::getData(),
+			"constraints" => [
+				new NotBlank(),
+				new Choice([
+					"choices" => DataTrait::getData(),
+				])
+			],
+			"attr" => [
+				"data-live-id" => "form-choices-2"
+			],
+			"empty_data" => "Element 0"
+		]);
+
+		$builder->add("date", DateType::class, [
+			"widget" => "single_text",
+			"label" => "Date",
+			"constraints" => [
+				new NotBlank(),
+				new Range([
+					"max" => "-13 years"
+				])
+			],
+			"attr" => [
+				"data-live-id" => "form-date-1"
 			]
 		]);
 	}
